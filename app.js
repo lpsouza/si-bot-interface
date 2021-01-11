da61be81171f669c51460c6faae51a8f3693366d
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2021-01-11 18:07:38 -0300

    Added log from conversation (for debugging)

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const bot = new Telegraf(TOKEN);
 
 bot.use(Telegraf.session());
 
+bot.use((context, next) => {
+    console.log(JSON.stringify(context, null, 2));
+    next();
+});
+
+
 bot.start((context) => context.reply('START OK'));
 
 bot.command('/test', (context) => context.reply('TEST OK'));
