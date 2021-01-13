e9af9ba12e6dbfaf24a45dba1085fdd1d898e0ec
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2021-01-13 00:38:31 -0300

    Changes in debug mode

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.use(Telegraf.session());
 
 bot.use((context, next) => {
-    if (process.env.DEBUG == 1) {
         let chatId = context.chat.id;
         let chatName = context.chat.title;
         let userId = context.from.id;
         let userName = context.from.first_name;
-        console.log(chatId, chatName, userId, userName);
+
+    if (process.env.DEBUG == 1) {
+        if (chatId == userId) {
+            console.log(`User: ${userName} (${userId}) access the bot direct`);
+        } else {
+            console.log(`User: ${userName} (${userId}) access the bot on ${chatName} (${chatId}) group`);
     }
     if (context.chat.id == process.env.GROUP_ID) {
         next();
