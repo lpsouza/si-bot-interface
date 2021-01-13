c79176947ba5793f29f0c9713b5ff6eb0fe98c21
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2021-01-13 00:43:15 -0300

    Changes onvalidation to use in group and show log to unauthorized access

--- app.js
+++ app.js
@@ -5,21 +5,24 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.use(Telegraf.session());
 
 bot.use((context, next) => {
-        let chatId = context.chat.id;
-        let chatName = context.chat.title;
-        let userId = context.from.id;
-        let userName = context.from.first_name;
+    let chatId = context.chat.id;
+    let chatName = context.chat.title;
+    let userId = context.from.id;
+    let userName = context.from.first_name;
 
     if (process.env.DEBUG == 1) {
         if (chatId == userId) {
             console.log(`User: ${userName} (${userId}) access the bot direct`);
         } else {
             console.log(`User: ${userName} (${userId}) access the bot on ${chatName} (${chatId}) group`);
+        }
     }
-    if (context.chat.id == process.env.GROUP_ID) {
+
+    if (context.chat.id.toString() == process.env.GROUP_ID) {
         next();
     }
     else {
+        console.log(`The user ${userName} has atempt to access the bot out of group.`);
         context.reply("NOT AUTHORIZED!");
     }
 });
