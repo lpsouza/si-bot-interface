6af7ed05339ed0661078b089b8a573f88d073050
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2023-05-18 18:25:28 -0300

    Improvements

--- app.js
+++ app.js
@@ -5,24 +5,27 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.use(Telegraf.session());
 
 bot.use((context, next) => {
-    let chatId = context.chat.id;
-    let chatName = context.chat.title;
-    let userId = context.from.id;
-    let userName = context.from.first_name;
+    console.log(JSON.stringify(context.chat, null, 2));
+    if (context.chat !== undefined) {
+        let chatId = context.chat.id;
+        let chatName = context.chat.title;
+        let userId = context.from.id;
+        let userName = context.from.first_name;
 
-    if (process.env.DEBUG == 1) {
-        if (chatId == userId) {
-            console.log(`User: ${userName} (${userId}) access the bot direct`);
-        } else {
-            console.log(`User: ${userName} (${userId}) access the bot on "${chatName}" (${chatId}) group`);
+        if (process.env.DEBUG == 1) {
+            if (chatId == userId) {
+                console.log(`User: ${userName} (${userId}) access the bot direct`);
+            } else {
+                console.log(`User: ${userName} (${userId}) access the bot on "${chatName}" (${chatId}) group`);
+            }
         }
-    }
 
-    if (context.chat.id.toString() == process.env.GROUP_ID) {
-        next();
-    }
-    else {
-        console.log(`The user ${userName} has atempt to access the bot out of group.`);
+        if (context.chat.id.toString() == process.env.GROUP_ID) {
+            next();
+        }
+        else {
+            console.log(`The user ${userName} has atempt to access the bot out of group.`);
+        }
     }
 });
 
